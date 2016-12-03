c48d6388862c716d46ff27cda1c46c03e9fb1473
laurahspencer <lhs3@uw.edu>
2016-12-02 23:59:09 -0800

    upate

---
+++ 2016-10_Geo-Ann-Project/scripts/move-gff.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+set -e
+set -u
+set -o pipefail
+
+# set variables
+GeoduckRepo="/Users/shlaura3/Documents/SAFS/FISH_546_Bioinformatics/546-Bioinformatics/2016-10_Geo-Ann-Project/"
+newdirectory="IGV_track_files"
+
+# change directory to Geoduck Annotation Project directory
+cd "$GeoduckRepo"
+
+# make new directory 
+mkdir "$newdirectory"
+
+# locate .gff files to move to newly created directory
+find ../2016-10_Geo-Ann-Project/analyses -name "*.gff" | xargs -I '0' mv '0' "$newdirectory"
+
+# display confirmation of move
+echo $?
+if [$? -eq 0];
+then
+	echo .gff files moved! && pwd && ls $newdirectory
+else
+	echo "Unsuccessful! try again."
+fi 
---
+++ Ostrea-Methylation/data/Untitled.md
@@ -0,0 +1,17 @@
+Manchester Project Plan:
+  * December - Pacifics & Olys (F2s, F1s)
+    * Maintain in 2 temperatures: cold & ambient for 2 months
+    * Move into 2 pH treatments (1 temp)
+    * Spawn 
+    
+ * Neptune Apex Aquacontrollers - to program dynamic temperature controller.
+ * Get $49 HOBO's - Make sure to set HOBO temperature loggers to start @ the same time using the "delay set" 
+ * 
+ 
+ Shiny apps -> browser interface for R-Studio 
+ 
+ Notse 
+ 
+ ! sudo perl /usr/local/RepeatMasker/RepeatMasker \
+-species "crassostrea gigas" -gff -s -dir ../analyses/RepeatMasker/ \
+../data/Panopea_generosa_scaff-70k.scafSeq
